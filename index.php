33acf207e62a8d5cf3066fab3be7260ee3dabc0d
Joshua <116948312+Creative-NH@users.noreply.github.com>
2024-02-02 17:31:06 +0800

    Fixed bugs and removed debug feature

--- index.php
+++ index.php
@@ -8,19 +8,6 @@ include("header.php");
 
 <?php
 
-if (!isset($_SESSION["ShopperID"])) {
-     echo "<form action='initialization.php' method='post'>";
-     echo "<input type='hidden' name='action' value='lg' />";
-     echo "&nbsp;&nbsp;<button type='submit'>Quick Login as User 1</button>";
-     echo "</form>";
-} else {
-     echo "<p>Product 1</p>";
-     echo "<form action='cartFunctions.php' method='post'>";
-     echo "<input type='hidden' name='action' value='add' />";
-     echo "<input type='hidden' name='product_id' value='1' />";
-     echo "Quantity: <input type='number' name='quantity' value='1' min='1' max='10' style='width:40px' required />";
-     echo "&nbsp;&nbsp;<button type='submit'>Add to Cart</button>";
-}
 // Include the Page Layout footer
 include("footer.php");
 ?>
