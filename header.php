718b48acefc9a9a37db8a92659b2a1c2d27bfd90
warrenkenzie <warren.kenzie28@gmail.com>
2024-02-02 15:05:42 +0800

    made the index page look nicer and added pictures

--- header.php
+++ header.php
@@ -1,29 +1,32 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Name of business</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="css/site.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <a href="index.php">
-                        <img src="Images/Global/logo.png" alt="logo" class="img-fluid" style="width:100%">
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12">
-                    <?php include("navbar.php"); ?>
-                </div>
-            </div>
-            <div class="col-sm-12" style="padding:15px;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Name of business</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/site.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="index.php">
+                    <img src="Images/PetalCraft_Banner.png" alt="logo" class="img-fluid" style="width:100%">
+                </a>
             </div>
         </div>
-    </body>
+        <div class="row">
+            <div class="col-sm-12">
+                <?php include("navbar.php"); ?>
+            </div>
+        </div>
+        <div class="col-sm-12" style="padding:15px;">
+        </div>
+    </div>
+</body>
+
 </html>
